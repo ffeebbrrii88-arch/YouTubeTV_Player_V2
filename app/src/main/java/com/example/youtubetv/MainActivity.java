11b325bc9d920e866af07db85e58da682aa2ad17
ffeebbrrii88-arch <ffeebbrrii88@gmail.com>
2026-09-09 00:11:31 +0700

    Initial YouTubeTV Player V2 Android TV project

---
+++ app/src/main/java/com/example/youtubetv/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.youtubetv;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView tv = new TextView(this);
+
+        tv.setText(
+            "YouTube TV Player V2\n\nSmartTube UI Migration"
+        );
+
+        tv.setTextSize(28);
+
+        setContentView(tv);
+    }
+}
