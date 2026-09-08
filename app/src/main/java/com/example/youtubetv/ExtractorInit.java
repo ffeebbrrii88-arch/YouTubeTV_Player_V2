d0ad15d7dee8a57d32f9b9fd755e0a321d7ed086
ffeebbrrii88-arch <ffeebbrrii88@gmail.com>
2026-09-09 01:12:42 +0700

    Temporary remove NewPipe extractor dependency for build

--- app/src/main/java/com/example/youtubetv/ExtractorInit.java
+++ app/src/main/java/com/example/youtubetv/ExtractorInit.java
@@ -1,26 +1,8 @@
 package com.example.youtubetv;
 
-import org.schabi.newpipe.extractor.NewPipe;
-import org.schabi.newpipe.extractor.localization.ContentCountry;
-import org.schabi.newpipe.extractor.localization.Localization;
-
-
 public class ExtractorInit {
 
-
-    public static void init(){
-
-        NewPipe.init(
-                new DownloaderImpl()
-        );
-
-
-        NewPipe.setLocalization(
-                new Localization(
-                        "en",
-                        ContentCountry.DEFAULT
-                )
-        );
-
+    public static void init() {
+        // NewPipe Extractor akan ditambahkan sebagai module nanti
     }
 }
--- app/src/main/java/com/example/youtubetv/YouTubeSearch.java
+++ app/src/main/java/com/example/youtubetv/YouTubeSearch.java
@@ -1,69 +1,19 @@
 package com.example.youtubetv;
 
-
-import org.schabi.newpipe.extractor.ServiceList;
-import org.schabi.newpipe.extractor.search.SearchExtractor;
-import org.schabi.newpipe.extractor.search.SearchInfo;
-
-
 import java.util.ArrayList;
 import java.util.List;
 
-
-
 public class YouTubeSearch {
 
+    public static List<VideoItem> search(String query) {
 
-    public static List<VideoItem> search(
-            String text
-    ){
+        List<VideoItem> list = new ArrayList<>();
 
-        List<VideoItem> list =
-                new ArrayList<>();
-
-
-        try {
-
-
-            SearchExtractor extractor =
-                    ServiceList.YouTube
-                    .getSearchExtractor(
-                            text,
-                            null,
-                            null
-                    );
-
-
-            extractor.fetchPage();
-
-
-            SearchInfo info =
-                    SearchInfo.getInfo(
-                            extractor
-                    );
-
-
-            info.getRelatedItems()
-                    .forEach(item -> {
-
-
-                        list.add(
-                                new VideoItem(
-                                        item.getName(),
-                                        "",
-                                        item.getThumbnailUrl()
-                                )
-                        );
-
-                    });
-
-
-        }catch(Exception e){
-
-            e.printStackTrace();
-
-        }
-
+        list.add(new VideoItem(
+                "Demo YouTube Video",
+                "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+                "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg"
+        ));
 
         return list;
     }
